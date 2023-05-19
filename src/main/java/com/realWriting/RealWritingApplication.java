1ca8f30ade670d4017096dc2a70016b47f278ecf
Kyungrim Park <pkl4693@naver.com>
2023-05-19 18:55:53 +0900

    initial setting

---
+++ src/main/java/com/realWriting/RealWritingApplication.java
@@ -0,0 +1,13 @@
+package com.realWriting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealWritingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealWritingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/realWriting/RealWritingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realWriting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealWritingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
