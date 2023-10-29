88fead7f83df958702ff55963ed8bb2c90d6cb92
Kyungrim Park <pkl4693@naver.com>
2023-10-29 20:02:03 +0900

    config : jwt

--- src/main/java/com/realWriting/global/config/SecurityConfig.java
+++ src/main/java/com/realWriting/global/config/SecurityConfig.java
@@ -1,30 +1,53 @@
 package com.realWriting.global.config;
 
+import com.realWriting.global.security.filter.JwtAuthenticationFilter;
+import com.realWriting.global.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final CorsFilter corsFilter;
+    private final JwtUtil jwtUtil;
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web -> web
+                .ignoring()
+                .requestMatchers("/user", "/login",
+                        /* swagger v3 */
+                        "/v3/api-docs/**",
+                        "/swagger-ui/**"));
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
+        http
                 .formLogin((f) -> f.disable())
                 .httpBasic((h) -> h.disable())
                 .csrf((c) -> c.disable())
                 .sessionManagement((s) -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .build();
+                .addFilterBefore(new JwtAuthenticationFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
+                .addFilter(corsFilter)
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated());
+        return http.build();
     }
 
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
